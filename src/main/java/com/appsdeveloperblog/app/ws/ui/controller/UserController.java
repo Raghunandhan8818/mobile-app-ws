07c6729de115c4127681bd27408ff18045b45767
Raghunandhan AJ <raghunandhan.19cs@kct.ac.in>
2024-04-18 12:05:05 +0530

    implemented getUsers with QueryParams

--- src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -6,23 +6,32 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("users")
 public class UserController {
 
+    @GetMapping(path = {"/{userID}"})
+    public String getUsers(@PathVariable String userID) {
+        return "Get Users Called" + userID;
+    }
+
     @GetMapping
-    public String getUsers() {
-        return "Get Users Called";
+    public String getUser(@RequestParam(value = "page", defaultValue = "1") int page, @RequestParam(value = "limit", defaultValue = "4545") int limit, @RequestParam(value = "sort", required = false) String sort) {
+        if(sort != null) {
+            return "Get Users Called" + String.valueOf(page) + String.valueOf(limit) + sort;
+        }else{
+            return "Get Users Called" + String.valueOf(page) + String.valueOf(limit) ;
+        }
     }
 
     @PostMapping
-    public String createUser(){
+    public String createUser() {
         return "Create User Called";
     }
 
     @PutMapping
-    public String updateUser(){
+    public String updateUser() {
         return "Update User Called";
     }
 
     @DeleteMapping
-    public String deleteUser(){
+    public String deleteUser() {
         return "Delete User Called";
     }
 
